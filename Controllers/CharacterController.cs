8e7076014a249325b551160c932199a6eb6a2194
Lucky Pathan <lckpathan@gmail.com>
2024-08-04 14:26:45 +0530

    Added Attribute Routing with mutiple GET request

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -11,10 +11,19 @@ namespace webAPT_DEMO.Controllers
     [Route("api/Character")]
     public class CharacterController : ControllerBase
     {
-        private static Character knight = new Character();
+        private static List<Character> characters  = new List<Character> {
+            new Character(),
+            new Character { Name = "Sam"}
+        };
+        
+        [HttpGet("GetAll")]
+        public ActionResult<List<Character>> Get(){
+            return Ok(characters);
+        }
+
         [HttpGet]
-        public ActionResult<Character> Get(){
-            return Ok(knight);
+        public ActionResult<Character> GetSingle(){
+            return Ok(characters[0]);
         }
     }
 }
