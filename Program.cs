310eabaa5ed2b9c223451ed9ba282526a768f89e
Lucky Pathan <lckpathan@gmail.com>
2024-08-05 11:47:50 +0530

    AutoMapper Added

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 global using webAPT_DEMO.Models;
 global using webAPT_DEMO.Services.CharacterServices;
 global using webAPT_DEMO.Dtos.Character;
+global using AutoMapper;
+
+using System.Reflection;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,6 +12,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 builder.Services.AddScoped<ICharacterServices,CharacterService>();
 
 var app = builder.Build();
--- Services/CharacterServices/CharacterService.cs
+++ Services/CharacterServices/CharacterService.cs
@@ -11,19 +11,27 @@ namespace webAPT_DEMO.Services.CharacterServices
             new Character(),
             new Character { Id = 1, Name = "Sam"}
         };
+        private readonly IMapper _mapper;
+
+        public CharacterService(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
 
         public async Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter)
         {
             var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
-            characters.Add(newCharacter);
-            serviceResponse.Data = characters;
+            var character = _mapper.Map<Character>(newCharacter);
+            character.Id = characters.Max(c => c.Id) + 1;
+            characters.Add(character);
+            serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
             return  serviceResponse;
         }
 
         public async Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters()
         {
             var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
-            serviceResponse.Data = characters;
+            serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
              return serviceResponse;
 
         }
@@ -33,7 +41,7 @@ namespace webAPT_DEMO.Services.CharacterServices
             var serviceResponse = new ServiceResponse<GetCharacterDto>();
 
             var character = characters.FirstOrDefault(c => c.Id == id);
-            serviceResponse.Data = character;
+            serviceResponse.Data = _mapper.Map<GetCharacterDto>(character);
             return serviceResponse;
         }
     }
